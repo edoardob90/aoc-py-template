6992167de3f167cfdfbd8406ebb579e917e00b8c
edoardob90 <edoardob90@gmail.com>
2023-10-27 17:56:30 +0200

    Use only positional args; auto-fetch puzzle name

--- entrypoint.py
+++ entrypoint.py
@@ -2,40 +2,54 @@
 from datetime import datetime
 
 import click
+import requests
+from bs4 import BeautifulSoup
+from requests.exceptions import HTTPError
 
 from copier import run_copy
+
+
+def fetch_puzzle_name(year: int, day: int) -> str:
+    try:
+        response = requests.get(f"https://adventofcode.com/{year}/day/{day}")
+        response.raise_for_status()
+    except HTTPError:
+        pass
+    else:
+        soup = BeautifulSoup(response.text, "html.parser")
+        if title := soup.find("h2"):
+            return title.text.replace("---", "").split(":", 1)[1].strip()
+    return ""
 
 
 @click.command()
 @click.argument("year", type=int, default=datetime.now().year)
 @click.argument("day", type=int, default=1)
-@click.option(
-    "--year-dir/--no-year-dir",
-    show_default=True,
+@click.argument(
+    "year_dir",
+    type=bool,
     default=False,
-    help="Whether the output path will contain the year folder",
 )
-@click.option(
-    "--src",
-    "-s",
+@click.argument(
+    "src",
     type=click.Path(file_okay=False, dir_okay=True),
-    default="template/",
-    show_default=True,
-    help="Source path",
+    default="/app/template/",
 )
-@click.option(
-    "--output",
-    "-o",
+@click.argument(
+    "output",
     type=click.Path(file_okay=False, dir_okay=True),
-    default="output/",
-    show_default=True,
-    help="Destination path",
+    default=".",
 )
 def entry_point(year: int, day: int, year_dir: bool, src: str, output: str) -> None:
     run_copy(
         src_path=src,
         dst_path=output,
-        data={"year": year, "day": day, "year_dir": year_dir, "puzzle_name": ""},
+        data={
+            "year": year,
+            "day": day,
+            "year_dir": year_dir,
+            "puzzle_name": fetch_puzzle_name(year, day),
+        },
         unsafe=True,
     )
 
